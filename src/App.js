7c662827bfa6d00b6f173d34eb0ab07512d67ad0
ChadDiaz <chadcdiaz@gmail.com>
2020-10-29 21:17:04 -0400

    setting up file struction and have my data api request done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react'
+import UserCard from './Components/UserCard';
+import './App.css'
+
 
 class App extends React.Component {
   constructor() {
     super();
+    console.log("cd: App.js: App: constructor-> was ran")
     this.state= {
-      
+      userCard: [],
+      userFollowing: [],
     }
   }
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/ChadDiaz')
+    .then((res) => res.json())
+    .then((json) => {
+      console.log("cd: App.js: App: CDM: userCard json results", json)
+      this.setState({
+        userCard: json
+      })
+    })
+    fetch('https://api.github.com/users/ChadDiaz/following')
+    .then((res) => res.json())
+    .then((json) => {
+      console.log("cd: App.js: App: CDM: userFollowing results", json)
+    })
+  }  
   render() {
     return(
-      <div>
-
+      <div className="App">
+        <UserCard userCard={this.state.userCard} />
       </div>
     );
   }
---
+++ src/Components/UserFollowers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserFollowers = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserFollowers
